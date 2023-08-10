55eebd7dbee98da2d755024d032ea31c047e3b59
bora <nbora617@naver.com>
2023-08-10 18:13:21 +0900

    상품페이지

--- script/login.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelectorAll('fieldset > div')
-console.log(input)
-for(let i of input){
-    i.addEventListener('click',function(){
-        i.style.border = '1px solid #BB2649'
-    })
-}
